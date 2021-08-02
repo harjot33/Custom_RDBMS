88b1723ffe89f24d5d7c14f7406404faa33f3bb8
Arunkumar Subash Gauda <ar822955@dal.ca>
2021-08-02 23:05:14 +0530

    Create

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
+
 public class run {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        Action action = new Action();
+        action.CreateDatabase("data1");
     }
 }
